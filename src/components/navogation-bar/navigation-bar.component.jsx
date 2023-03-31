37468126155860874d56cf2105e2a7b15a3b6e89
project3cs <project3cs123@gmail.com>
2023-03-31 23:38:08 +0530

    created navigation bar

---
+++ src/components/navogation-bar/navigation-bar.component.jsx
@@ -0,0 +1,12 @@
+// styles
+import "./navigation-bar.styles.css";
+
+const NavigationBar = () => {
+  return (
+    <div className="navigation-bar">
+      <h3 className="navigation-bar-title">Title</h3>
+    </div>
+  );
+};
+
+export default NavigationBar;
