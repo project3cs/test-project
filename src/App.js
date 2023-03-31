fb549e393558368bc9c0df34d513b1727ebe253c
project3cs <project3cs123@gmail.com>
2023-03-31 23:07:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// importing react-router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+// my compoenents
+import Home from "./layouts/home/home.layout";
+import Users from "./layouts/users/users.layout";
+import UserDetails from "./components/user-details/user-details.component";
+
+// styles
+import "./App.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route index element={<Users />} />
+      {/* <Route path="shop/*" element={<Shop />} />{" "} */}
+      <Route path="users/:userID" element={<UserDetails />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
